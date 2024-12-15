2084666c8b2344fbc8aeab3dc348f6ad1ad87e38
jainamzaveri <JainamSanjay.Zaveri01@student.csulb.edu>
2024-12-15 15:14:56 -0800

    trainer.py

--- trainer.py
+++ trainer.py
@@ -5,7 +5,7 @@ class Trainer:
         self.data = data
         self.model = model
         self.optimizer = optimizer
-        def train(self, iterations, batch_size, block_size):
+    def train(self, iterations, batch_size, block_size):
         for iter in range(iterations):
             xb, yb = self.data.get_batch('train', batch_size, block_size)
             logits, loss = self.model(xb, yb)
@@ -13,3 +13,5 @@ class Trainer:
             loss.backward()
             self.optimizer.step()
             if iter % 100 == 0:
+                print(f"Step {iter}, Loss: {loss.item()}")
+                 
