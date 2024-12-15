ef1fe81016f619bd075643752f0cb8780ff97db9
jainamzaveri <JainamSanjay.Zaveri01@student.csulb.edu>
2024-12-15 15:10:54 -0800

    trainer.py

--- trainer.py
+++ trainer.py
@@ -5,3 +5,5 @@ class Trainer:
         self.data = data
         self.model = model
         self.optimizer = optimizer
+        def train(self, iterations, batch_size, block_size):
+        for iter in range(iterations):
