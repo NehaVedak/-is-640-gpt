6e994d3c2e99594d2fe54aa7ff15cd35e377718e
MEHUL NIKUMBH <76685911+mehulnikumbh@users.noreply.github.com>
2024-12-15 16:22:09 -0800

    Add files via upload

--- model.py
+++ model.py
@@ -2,11 +2,11 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-
-batch_size = 16 
-block_size = 8 
+# hyperparameters
+batch_size = 16 # how many independent sequences will we process in parallel?
+block_size = 8 # what is the maximum context length for predictions?
 max_iters = 100
-eval_interval = 500
+eval_interval = 10
 learning_rate = 0.001
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
@@ -14,7 +14,7 @@ n_embd = 32
 n_head = 4
 n_layer = 3
 dropout = 0.2
-
+# ------------
 
 torch.manual_seed(1337)
 
